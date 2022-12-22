c685e521705db51821d35dd6b1b1941fced96f0d
anasazamov <anasazamov55@gmail.com>
2022-12-22 16:55:24 +0500

    answers

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,7 +1,4 @@
 #Create a varaible 'number' and assign it the value of 43.
-import numbers
-
-
 number=43
 #Create a variable 'answer' and assign it the sum of the number's digits.
 answer=number//10+number%10
